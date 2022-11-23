a1ea3d7a637a320e3435a0416b659348a77502ed
Flippette <quangdatle2006@outlook.com>
2022-11-24 02:17:00 +0700

    Fixed incorrect stepping logic, eliminated pre-main code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+#![no_main]
+
 use image::{ImageOutputFormat, Luma};
 use rayon::prelude::*;
-use std::{fs::File, io::BufWriter, time::Instant};
+use std::{
+    fs::File,
+    io::{self, BufWriter, Write},
+    time::Instant,
+};
 
 mod complex;
 use complex::Complex;
@@ -13,11 +19,14 @@ const Y_OFFSET: i32 = 0;
 const X_OFFSET: i32 = -VIEWPORT_HEIGHT / 4;
 const SCALE: f32 = 0.0001;
 
-fn main() {
-    eprintln!("[info] rendering started.");
+#[no_mangle]
+pub fn main() {
+    let mut stderr = io::stderr().lock();
+
+    stderr.write(b"[info] rendering started.\n").unwrap();
     let timer = Instant::now();
 
-    let upper_half = (-VIEWPORT_HEIGHT / 2..0)
+    let mut output = (-VIEWPORT_HEIGHT / 2..0)
         .into_par_iter()
         .flat_map_iter(|row| {
             (-VIEWPORT_WIDTH / 2 + X_OFFSET..VIEWPORT_WIDTH / 2 + X_OFFSET)
@@ -25,24 +34,24 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let mut output = Vec::with_capacity(VIEWPORT_WIDTH as usize * VIEWPORT_HEIGHT as usize);
+    output.reserve_exact(VIEWPORT_WIDTH as usize * VIEWPORT_HEIGHT as usize);
 
-    (0..VIEWPORT_HEIGHT as usize / 2)
-        .map(|i| &upper_half[i * VIEWPORT_WIDTH as usize..(i + 1) * VIEWPORT_WIDTH as usize])
-        .for_each(|slice| output.extend(slice.iter()));
+    (0..VIEWPORT_HEIGHT as usize / 2).rev().for_each(|start| {
+        output.extend_from_within(
+            start * VIEWPORT_WIDTH as usize..(start + 1) * VIEWPORT_WIDTH as usize,
+        )
+    });
 
-    (0..VIEWPORT_HEIGHT as usize / 2)
-        .rev()
-        .map(|i| &upper_half[i * VIEWPORT_WIDTH as usize..(i + 1) * VIEWPORT_WIDTH as usize])
-        .for_each(|slice| output.extend(slice.iter()));
-
-    eprintln!(
-        "[info] rendering took {:.4} seconds, writing to output file...",
-        timer.elapsed().as_secs_f32()
-    );
+    stderr.write(b"[info] rendering took ").unwrap();
+    stderr
+        .write(timer.elapsed().as_secs_f32().to_string().as_bytes())
+        .unwrap();
+    stderr
+        .write(b" seconds, writing to output file...\n")
+        .unwrap();
 
     let mut writer = BufWriter::new(File::create("image.jpeg").unwrap());
-    let image: image::ImageBuffer<Luma<u8>, Vec<_>> =
+    let image: image::ImageBuffer<Luma<u8>, _> =
         image::ImageBuffer::from_vec(VIEWPORT_WIDTH as u32, VIEWPORT_HEIGHT as u32, output)
             .unwrap();
 
@@ -50,22 +59,26 @@ fn main() {
         .write_to(&mut writer, ImageOutputFormat::Jpeg(100))
         .unwrap();
 
-    eprintln!("[info] done.");
+    stderr.write(b"[info] done.\n").unwrap();
 }
 
 #[inline(always)]
 fn render(x: f32, y: f32) -> u8 {
     let c = Complex::new(x, y);
-    let mut z = c;
+    let mut z2 = c;
 
     #[allow(unused_assignments)]
-    let mut last_z = z;
+    let mut z1 = z2;
 
     for iter in (1..ITER_MAX).step_by(2) {
-        last_z = z;
-        z = z.sqr() + c;
-        if z.re.is_nan() {
-            return ITER_MAX - iter - last_z.re.is_nan() as u8;
+        // 1,2 -> 3,4 -> ..
+        z1 = z2.sqr() + c;
+        z2 = z1.sqr() + c;
+
+        match (z1.re.is_nan(), z2.re.is_nan()) {
+            (_, true) => return ITER_MAX - iter,
+            (true, _) => return ITER_MAX - iter - 1,
+            _ => continue,
         }
     }
 
